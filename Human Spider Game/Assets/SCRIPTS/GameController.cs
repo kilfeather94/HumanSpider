4e2e92b676aac4502a18fdd0088bd1b7c7915abf
Michael Kilfeather <michaelkilfeather94@gmail.com>
2018-02-03 00:22:44 +0000

    GameController.cs script added
    Script added for basic button mashing mechanic. Needs some adjusting

---
+++ Human Spider Game/Assets/SCRIPTS/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+ 
+    public float currStrength;
+    public float maxStrength;
+    public float recoveryRate;
+
+    public Slider content;
+
+    void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+    
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            content.value += 3.0f;
+        }
+            
+        HandleBar();
+    }
+
+    private void HandleBar()
+    {
+        if(content.value != maxStrength)
+        {
+            content.value = Mathf.MoveTowards(content.value, maxStrength, recoveryRate * Time.deltaTime);
+        }
+    }
+}
