be6e907525ca537f4a9c1d99e0c23d7e5cd2d2d1
Michael Kilfeather <michaelkilfeather94@gmail.com>
2018-02-04 13:31:09 +0000

    MUSIC files added, SplashScreen added

---
+++ Human Spider Game/Assets/SceneFade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneFade : MonoBehaviour
+{
+
+    public Image splashScreen;
+    private Image fadeBlack;
+
+	void Start ()
+    {
+        fadeBlack = GetComponent<Image>();
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+
+    public void DisableSplashScreen()
+    {
+        splashScreen.enabled = false;
+    }
+}
